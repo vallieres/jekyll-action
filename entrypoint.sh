a478221f45cd752e63570eb3d4bd33e5f628919c
Leon Aves <leon@fiit.tv>
2020-04-27 00:25:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,53 +1,12 @@
 #!/bin/sh
 set -e
 
-echo "Starting the Jekyll Action"
-
-echo "::debug ::Starting bundle install"
+echo "Installing gems"
 bundle config path vendor/bundle
 bundle install
-echo "::debug ::Completed bundle install"
+echo "Gems installed"
 
-if [[ ${INPUT_JEKYLL_SRC} ]]; then
-  JEKYLL_SRC=${INPUT_JEKYLL_SRC}
-  echo "::debug ::Using parameter value ${JEKYLL_SRC} as a source directory"
-elif [[ ${SRC} ]]; then
-  JEKYLL_SRC=${SRC}
-  echo "::debug ::Using SRC environment var value ${JEKYLL_SRC} as a source directory"
-else
-  JEKYLL_SRC=$(find . -path ./vendor/bundle -prune -o -name '_config.yml' -exec dirname {} \;)
-  echo "::debug ::Resolved ${JEKYLL_SRC} as a source directory"
-fi
-
-bundle exec jekyll build -s ${JEKYLL_SRC} -d build
+echo "Jekyll building"
+bundle exec jekyll build
 echo "Jekyll build done"
 
-cd build
-
-# No need to have GitHub Pages to run Jekyll
-touch .nojekyll
-
-# Is this a regular repo or an org.github.io type of repo
-if [[ "${GITHUB_REPOSITORY}" == *".github.io"* ]]; then
-  remote_branch="master"
-else
-  remote_branch="gh-pages"
-fi
-
-if [ "${GITHUB_REF}" == "refs/heads/${remote_branch}" ]; then
-  echo "Cannot publish on branch ${remote_branch}"
-  exit 1
-fi
-
-echo "Publishing to ${GITHUB_REPOSITORY} on branch ${remote_branch}"
-
-remote_repo="https://${JEKYLL_PAT}@github.com/${GITHUB_REPOSITORY}.git" && \
-git init && \
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git add . && \
-git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
-git push --force $remote_repo master:$remote_branch && \
-rm -fr .git && \
-cd ..
-exit 0
